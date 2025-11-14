c4fdc072539dd9495cc5198f8548e1d957e1895e
WhakE <83140809+WhakEi@users.noreply.github.com>
2025-11-15 01:18:50 +0300

    Update app version from 0.7.0 to 0.9.0

--- Riyadh Transport/Views/SettingsView.swift
+++ Riyadh Transport/Views/SettingsView.swift
@@ -43,7 +43,7 @@ struct SettingsView: View {
                     HStack {
                         Text("version")
                         Spacer()
-                        Text("0.7.0")
+                        Text("0.9.0")
                             .foregroundColor(.secondary)
                     }
                     Link(destination: URL(string: "https://github.com/WhakEi/Riyadh-Transport-iOS")!) {
